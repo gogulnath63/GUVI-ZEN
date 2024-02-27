6dbcdf044b098b3685f7bfeea7331d4ef70d7d5a
gogulnath63 <gogulnath6383@gmail.com>
2024-02-27 09:46:51 +0530

    js data types

---
+++ JS-basic/script.js
@@ -0,0 +1,12 @@
+// data types
+
+//primitive datatypes
+
+//numbers
+console.log(typeof 5); // interger or float values
+//string
+console.log(typeof "5"); // chachter encode in " or "
+//boolean
+console.log(typeof true); // true or false
+
+
